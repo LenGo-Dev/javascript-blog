ebc1b898b85db11dc3666c347d1fa5a73085f97d
LenGo-Dev <lena.gotovska@gmail.com>
2025-08-05 14:26:10 +0200

    Add class 'active' to the clicked link

--- js/script.js
+++ js/script.js
@@ -6,17 +6,20 @@
 
 const titleClickHandler = function(event){
   console.log('Link was clicked!');
+  const clickedElement = this;
  console.log(event);
 
-  /* remove class 'active' from all article links  */
+  /* [DONE] remove class 'active' from all article links  */
   const activeLinks = document.querySelectorAll('.titles a.active');
 
   for(let activeLink of activeLinks){
     activeLink.classList.remove('active');
   }
-  /* add class 'active' to the clicked link */
+  /* [DONE] add class 'active' to the clicked link */
+  console.log('clickedElement:', clickedElement);
+  clickedElement.classList.add('active');
 
-  /* remove class 'active' from all articles */
+  /* [DONE] remove class 'active' from all articles */
   const activeArticles = document.querySelectorAll('.posts .active');
 
   for(let activeArticle of activeArticles){
