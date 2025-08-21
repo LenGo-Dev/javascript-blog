cda3e6c2990691c23754e0411a9fe72c1e8632f6
LenGo-Dev <lena.gotovska@gmail.com>
2025-08-21 17:31:14 +0200

    Add description to global Handlebars

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 'use strict';
+/* global Handlebars */
 const templates = {
   articleLink: Handlebars.compile(document.querySelector('#template-article-link').innerHTML),
   tagLink: Handlebars.compile(document.querySelector('#template-tag-link').innerHTML),
